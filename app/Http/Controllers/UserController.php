17508b8465a3293a05077b4c322bd4a1c793a961
nikolin-anton <antonnikolin@gmail.com>
2023-08-29 16:10:40 +0300

    delete not used function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,28 +24,4 @@ class UserController extends Controller
     {
         return UserResource::make(User::create($request->validated()));
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
